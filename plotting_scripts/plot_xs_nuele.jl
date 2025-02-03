3d2376386c729a76bad80318046a61b6fdb8044f
kcarloni <kcarloni@g.harvard.edu>
2025-02-03 17:55:46 -0500

    figure

--- plotting_scripts/plot_xs_nuele.jl
+++ plotting_scripts/plot_xs_nuele.jl
@@ -35,27 +35,38 @@ end
 
 begin
 # full energy range
-    fig = Figure()
-    ax = Axis( fig[1,1], yscale=log10, xscale=log10 )
+    u_x = 1GeV
+    u_y = 1cm^2
 
-    u_x = GeV
-    u_y = cm^2
+    fig = Figure()
+    ax = Axis( fig[1,1], yscale=log10, xscale=log10,
+        xlabel="neutrino energy $(unit(u_x))",
+        ylabel="cross-section $(unit(u_y))"
+    )
 
     x = exp10.(-3:0.05:12) * GeV
 
     for (i, nu) in enumerate( Particle.( [12, -12, 14, -14, 16, -16] ) )
 
         y = σ_tot_nuele.( x, nu )
-        c = Cycled(i)
+        c = distinct_sequential[12][i+2] #Cycled(i)
+
+        if nu.pdgid.value < 0;
+            ls=(:dash, :dense)
+        else;
+            ls=:solid
+        end
 
         lines!( ax, x/u_x, y/u_y, 
             label="$(nu.glyph)",
-            color=c
+            color=c,
+            linestyle=ls
         )
     end
     vlines( 11GeV/u_x )
 
-    axislegend(ax)
+    axislegend(ax, position=:lt)
+    save( "figures/xs_nuele.pdf", fig )
     fig
 end
 
